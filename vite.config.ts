7a140ec7ebb01aa8985f4c79c65e20f7b9e335d0
Lisoing Sem <lisoingsem@gmail.com>
2026-02-17 02:54:58 +0700

    feat: Integrate NativePHP mobile plugin, configure Laravel hot file, and add `@` alias for resources.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import path from 'path';
+import { nativephpHotFile, nativephpMobile } from './vendor/nativephp/mobile/resources/js/vite-plugin.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources'),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/app.ts'],
             ssr: 'resources/app.ts',
             refresh: true,
+            hotFile: nativephpHotFile(),
         }),
+        nativephpMobile(),
         tailwindcss(),
         wayfinder({
             formVariants: true,
