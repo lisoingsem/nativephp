284c46a6e425e24d45649129190c36e890ee7d3d
Lisoing Sem <lisoingsem@gmail.com>
2026-02-17 02:28:39 +0700

    feat: Introduce new agent skills for Inertia Vue, Tailwind CSS, Wayfinder, Fortify, and Pest testing, along with corresponding configuration updates.

---
+++ config/nativephp.php
@@ -0,0 +1,76 @@
+<?php
+
+return [
+    /**
+     * The version of your app.
+     * It is used to determine if the app needs to be updated.
+     * Increment this value every time you release a new version of your app.
+     */
+    'version' => env('NATIVEPHP_APP_VERSION', '1.0.0'),
+
+    /**
+     * The ID of your application. This should be a unique identifier
+     * usually in the form of a reverse domain name.
+     * For example: com.nativephp.app
+     */
+    'app_id' => env('NATIVEPHP_APP_ID', 'com.nativephp.app'),
+
+    /**
+     * If your application allows deep linking, you can specify the scheme
+     * to use here. This is the scheme that will be used to open your
+     * application from within other applications.
+     * For example: "nativephp"
+     *
+     * This would allow you to open your application using a URL like:
+     * nativephp://some/path
+     */
+    'deeplink_scheme' => env('NATIVEPHP_DEEPLINK_SCHEME'),
+
+    /**
+     * The author of your application.
+     */
+    'author' => env('NATIVEPHP_APP_AUTHOR'),
+
+    /**
+     * The copyright notice for your application.
+     */
+    'copyright' => env('NATIVEPHP_APP_COPYRIGHT'),
+
+    /**
+     * The description of your application.
+     */
+    'description' => env('NATIVEPHP_APP_DESCRIPTION', 'An awesome app built with NativePHP'),
+
+    /**
+     * The Website of your application.
+     */
+    'website' => env('NATIVEPHP_APP_WEBSITE', 'https://nativephp.com'),
+
+    /**
+     * The default service provider for your application. This provider
+     * takes care of bootstrapping your application and configuring
+     * any global hotkeys, menus, windows, etc.
+     */
+    'provider' => \App\Providers\NativeServiceProvider::class,
+
+    // ... (rest of the mobile v3 config)
+    'server' => [
+        'http_port' => env('NATIVEPHP_HTTP_PORT', 3000),
+        'ws_port' => env('NATIVEPHP_WS_PORT', 8081),
+        'service_name' => env('NATIVEPHP_SERVICE_NAME', 'NativePHP Server'),
+        'service_type' => '_http._tcp',
+        'public_path' => env('NATIVEPHP_PUBLIC_PATH', 'public'),
+        'build_path' => env('NATIVEPHP_BUILD_PATH', 'storage/app/native-build'),
+        'open_browser' => env('NATIVEPHP_OPEN_BROWSER', true),
+        'watch_paths' => [
+            'app',
+            'resources',
+            'routes',
+            'public/build',
+        ],
+        'watch_extensions' => ['php', 'blade.php', 'js', 'css', 'ts', 'vue', 'json'],
+    ],
+
+    'permissions' => [
+    ],
+];
